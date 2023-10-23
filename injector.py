711a42271835a8199774979987475cffd4a67606
YaoChen19 <99479155+YaoChen19@users.noreply.github.com>
2023-10-23 02:42:44 +0200

    Update injector.py

--- injector.py
+++ injector.py
@@ -1,25 +1,3 @@
-"""
-import frida, sys
-
-jscodeFile = open("nohttpinterceptor.js", "r")
-jscode = jscodeFile.read()
-jscodeFile.close()
-
-def on_message(message, data):
-    if message['type'] == 'send':
-        print(message['payload'])
-    else:
-        print(message)
-
-device = frida.get_usb_device()
-process = device.attach("Twitter")
-script = process.create_script(jscode)
-script.on('message', on_message)
-
-print('[+] Running')
-script.load()
-sys.stdin.read()
-"""
 import frida
 import sys
 import os
